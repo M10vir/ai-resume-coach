0f868f45797d3b4ee134df5094cd470246331f29
M10vir <tanvirscsa@gmail.com>
2025-04-10 23:36:16 +0300

    Update App.jsx - using React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RAGSearch from './pages/RAGSearch';
+
+<Route path="/rag-search" element={<RAGSearch />} />
 
 function App() {
   const [count, setCount] = useState(0)
